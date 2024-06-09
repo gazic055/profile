582fc9202412456c64775a457c076ac02882d9ce
gazic055 <66394473+gazic055@users.noreply.github.com>
2024-06-09 16:46:21 -0400

    included the contact form

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+//displays the contact div
+function open_popup(){
+
+    let contact_div = document.getElementById("contact-popup");
+    contact_div.style.display = "block"; //none to hide, block to show
+
+}
+
+function close_popup(){
+    let contact_div = document.getElementById("contact-popup");
+    contact_div.style.display = "none";
+}
+
+function send_email(){
+    alert(document.getElementById("message-field").value);
+}
+
 function sleep(ms){
     return new Promise(resolve => setTimeout(resolve,ms ));
 }
@@ -9,11 +26,33 @@ function age_calculator(){
 
 async function insert_words(){
 
-    let words_list = [String(age_calculator()),"a developer","a designer","a problem-solver"];
+    let words_list = [String(age_calculator()),"designer","developer","problem-solver"];
     let html_line = document.getElementById("dynamic-words");
+    let html_age_line = document.getElementById("age-line");
+    let html_age_sentence = document.getElementById("age_sentence");
+    let html_dynamic_sentence = document.getElementById("dynamic-sentence");
 
     for(let i=0; i<4; i++){
-        html_line.textContent = words_list[i];
+        html_line.textContent = "";
+        html_age_line.textContent = "";
+
+        for(j=0; j<words_list[i].length; j++){
+
+            if(words_list[i] == "24"){
+                html_dynamic_sentence.style.display = "none";
+                html_age_sentence.style.display = "block"; //none to hide, block to show
+                html_age_line.innerHTML += words_list[i].charAt(j);
+                await sleep(70);
+            }
+            else{
+                html_age_sentence.style.display = "none";
+                html_dynamic_sentence.style.display = "block";
+                html_line.innerHTML += words_list[i].charAt(j);
+                await sleep(70);
+            }
+
+        }
+       // html_line.textContent = words_list[i];
         await sleep(1500)
         if(i == 3){
             //reset i back to -1. i++ will set it to 0 when checking the loop condition
